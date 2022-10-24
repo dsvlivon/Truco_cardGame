ff1c5a0ca0317e391e300640ade39c72cfcf328c
Vizgarra Livon <daniel.vizgarralivon@kcc.com>
2022-10-24 14:12:37 -0300

    finally i got 12 cards

---
+++ Entidades/SideBoard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class SideBoard
+    {
+        public static string basePath = "C:\\Users\\L54556\\OneDrive - Kimberly-Clark\\Desktop\\DANIEL\\Truco\\Media\\"; //this need to change
+        public static string dorso;
+        public static string cards;
+
+        static SideBoard()
+        {
+            dorso = basePath + "dorsos\\dorso0.jpg";
+            cards = basePath + "cartas asart\\";
+        }
+        
+    }
+}
